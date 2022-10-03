107db5fdde344cdc61f3c81aaf0c49dc194b5c71
Diego Zito <diegozito.m@gmail.com>
2022-10-03 13:31:04 +0200

    signUp

--- final-boiler-plate/src/stores/user.js
+++ final-boiler-plate/src/stores/user.js
@@ -20,5 +20,25 @@ export const useUserStore = defineStore("user", {
         console.log(this.user);
       }
     },
+    async signUp(email, password) {
+      const { user, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+      if (error) throw error;
+      if (user) {
+        this.user = user;
+        console.log(this.user);
+      }
+    },
+    persist: {
+      enabled: true,
+      strategies: [
+        {
+          key: "user",
+          storage: localStorage,
+        },
+      ],
+    },
   },
 });
